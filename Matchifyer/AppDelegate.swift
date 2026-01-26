5925d374111ae771e6d5afcd1bac95d8009e40cd
matchifyer-dev <hgmanbiz@gmail.com>
2026-01-25 22:22:26 -0500

    Create AppDelegate.swift

---
+++ Matchifyer/AppDelegate.swift
@@ -0,0 +1,74 @@
+import UIKit
+import FirebaseCore
+import FirebaseMessaging
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window : UIWindow?
+
+    func application(_ application: UIApplication,
+                       didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        Messaging.messaging().delegate = self
+        UNUserNotificationCenter.current().delegate = self
+        return true
+      }
+
+      func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        if let messageID = userInfo[gcmMessageIDKey] {
+          print("Message ID 1: \(messageID)")
+        }
+        print("push userInfo 1:", userInfo)
+        sendPushToWebView(userInfo: userInfo)
+      }
+
+      func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                       fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        if let messageID = userInfo[gcmMessageIDKey] {
+          print("Message ID 2: \(messageID)")
+        }
+        print("push userInfo 2:", userInfo)
+        sendPushToWebView(userInfo: userInfo)
+        completionHandler(UIBackgroundFetchResult.newData)
+      }
+
+      func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Unable to register for remote notifications: \(error.localizedDescription)")
+      }
+    }
+
+    extension AppDelegate : UNUserNotificationCenterDelegate {
+
+      func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                  willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        if let messageID = userInfo[gcmMessageIDKey] {
+          print("Message ID: 3 \(messageID)")
+        }
+        print("push userInfo 3:", userInfo)
+        sendPushToWebView(userInfo: userInfo)
+        completionHandler([[.banner, .list, .sound]])
+      }
+
+      func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                  didReceive response: UNNotificationResponse,
+                                  withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        if let messageID = userInfo[gcmMessageIDKey] {
+          print("Message ID 4: \(messageID)")
+        }
+        print("push userInfo 4:", userInfo)
+        sendPushClickToWebView(userInfo: userInfo)
+        completionHandler()
+      }
+    }
+
+    extension AppDelegate : MessagingDelegate {
+      func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("Firebase registration token: \(String(describing: fcmToken))")
+        let dataDict:[String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
+        handleFCMToken()
+      }
+    }
