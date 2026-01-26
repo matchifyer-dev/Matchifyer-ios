b77ab61efd7818106887532078d9d715756508bf
matchifyer-dev <hgmanbiz@gmail.com>
2026-01-25 22:34:09 -0500

    Create WebView.swift

---
+++ Matchifyer/WebView.swift
@@ -0,0 +1,157 @@
+import UIKit
+import WebKit
+import AuthenticationServices
+import SafariServices
+
+func createWebView(container: UIView, WKSMH: WKScriptMessageHandler, WKND: WKNavigationDelegate, NSO: NSObject, VC: ViewController) -> WKWebView{
+    let config = WKWebViewConfiguration()
+    let userContentController = WKUserContentController()
+    userContentController.add(WKSMH, name: "print")
+    userContentController.add(WKSMH, name: "push-subscribe")
+    userContentController.add(WKSMH, name: "push-permission-request")
+    userContentController.add(WKSMH, name: "push-permission-state")
+    userContentController.add(WKSMH, name: "push-token")
+    config.userContentController = userContentController
+    config.limitsNavigationsToAppBoundDomains = true;
+    config.allowsInlineMediaPlayback = true
+    config.preferences.javaScriptCanOpenWindowsAutomatically = true
+    config.preferences.setValue(true, forKey: "standalone")
+
+    let webView = WKWebView(frame: calcWebviewFrame(webviewView: container, toolbarView: nil), configuration: config)
+    setCustomCookie(webView: webView)
+    webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    webView.isHidden = true;
+    webView.navigationDelegate = WKND
+    webView.scrollView.bounces = false
+    webView.scrollView.contentInsetAdjustmentBehavior = .never
+    webView.allowsBackForwardNavigationGestures = true
+
+    if #available(iOS 16.4, macOS 13.3, *) { webView.isInspectable = true }
+
+    let deviceModel = UIDevice.current.model
+    let osVersion = UIDevice.current.systemVersion
+    webView.configuration.applicationNameForUserAgent = "Safari/604.1"
+    webView.customUserAgent = "Mozilla/5.0 (\(deviceModel); CPU \(deviceModel) OS \(osVersion.replacingOccurrences(of: ".", with: "_")) like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/\(osVersion) Mobile/15E148 Safari/604.1 PWAShell"
+    webView.addObserver(NSO, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: NSKeyValueObservingOptions.new, context: nil)
+    return webView
+}
+
+func setCustomCookie(webView: WKWebView) {
+    let _platformCookie = HTTPCookie(properties: [
+        .domain: rootUrl.host!,
+        .path: "/",
+        .name: platformCookie.name,
+        .value: platformCookie.value,
+        .secure: "FALSE",
+        .expires: NSDate(timeIntervalSinceNow: 31556926)
+    ])!
+    webView.configuration.websiteDataStore.httpCookieStore.setCookie(_platformCookie)
+}
+
+func calcWebviewFrame(webviewView: UIView, toolbarView: UIToolbar?) -> CGRect{
+    if ((toolbarView) != nil) {
+        return CGRect(x: 0, y: toolbarView!.frame.height, width: webviewView.frame.width, height: webviewView.frame.height - toolbarView!.frame.height)
+    } else {
+        let winScene = UIApplication.shared.connectedScenes.first
+        let windowScene = winScene as! UIWindowScene
+        var statusBarHeight = windowScene.statusBarManager?.statusBarFrame.height ?? 0
+        switch displayMode {
+        case "fullscreen":
+            #if targetEnvironment(macCatalyst)
+            if let titlebar = windowScene.titlebar { titlebar.titleVisibility = .hidden; titlebar.toolbar = nil }
+            #endif
+            return CGRect(x: 0, y: 0, width: webviewView.frame.width, height: webviewView.frame.height)
+        default:
+            #if targetEnvironment(macCatalyst)
+            statusBarHeight = 29
+            #endif
+            let windowHeight = webviewView.frame.height - statusBarHeight
+            return CGRect(x: 0, y: statusBarHeight, width: webviewView.frame.width, height: windowHeight)
+        }
+    }
+}
+
+extension ViewController: WKUIDelegate, WKDownloadDelegate {
+    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+        if (navigationAction.targetFrame == nil) { webView.load(navigationAction.request) }
+        return nil
+    }
+
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if (navigationAction.request.url?.scheme == "about") { return decisionHandler(.allow) }
+        if (navigationAction.shouldPerformDownload || navigationAction.request.url?.scheme == "blob") { return decisionHandler(.download) }
+
+        if let requestUrl = navigationAction.request.url {
+            if let requestHost = requestUrl.host {
+                let matchingAuthOrigin = authOrigins.first(where: { requestHost.range(of: $0) != nil })
+                if (matchingAuthOrigin != nil) {
+                    decisionHandler(.allow)
+                    if (toolbarView.isHidden) {
+                        toolbarView.isHidden = false
+                        webView.frame = calcWebviewFrame(webviewView: webviewView, toolbarView: toolbarView)
+                    }
+                    return
+                }
+
+                let matchingHostOrigin = allowedOrigins.first(where: { requestHost.range(of: $0) != nil })
+                if (matchingHostOrigin != nil) {
+                    decisionHandler(.allow)
+                    if (!toolbarView.isHidden) {
+                        toolbarView.isHidden = true
+                        webView.frame = calcWebviewFrame(webviewView: webviewView, toolbarView: nil)
+                    }
+                    return
+                }
+
+                if (navigationAction.navigationType == .other && navigationAction.value(forKey: "syntheticClickType") as! Int == 0 && (navigationAction.targetFrame != nil) && (navigationAction.sourceFrame != nil)) {
+                    decisionHandler(.allow)
+                    return
+                } else {
+                    decisionHandler(.cancel)
+                }
+
+                if ["http", "https"].contains(requestUrl.scheme?.lowercased() ?? "") {
+                    let safariViewController = SFSafariViewController(url: requestUrl)
+                    self.present(safariViewController, animated: true, completion: nil)
+                } else {
+                    if (UIApplication.shared.canOpenURL(requestUrl)) { UIApplication.shared.open(requestUrl) }
+                }
+            } else {
+                decisionHandler(.cancel)
+                if (navigationAction.request.url?.scheme == "tel" || navigationAction.request.url?.scheme == "mailto" ){
+                    if (UIApplication.shared.canOpenURL(requestUrl)) { UIApplication.shared.open(requestUrl) }
+                }
+            }
+        } else {
+            decisionHandler(.cancel)
+        }
+    }
+
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: { _ in completionHandler() })
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+
+    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in completionHandler(false) })
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: { _ in completionHandler(true) })
+        alert.addAction(cancelAction)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+
+    func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (String?) -> Void) {
+        let alert = UIAlertController(title: nil, message: prompt, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in completionHandler(nil) })
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: { _ in
+            if let input = alert.textFields?.first?.text { completionHandler(input) }
+        })
+        alert.addTextField { textField in textField.placeholder = defaultText }
+        alert.addAction(cancelAction)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+}
